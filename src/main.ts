8d2cb36e71a8a394988d6360b39b442de4651056
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-10-14 13:31:37 -0700

    step one completed, added white canvas with border, rounded edges, and black border

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import "./style.css";
 
-const APP_NAME = "New Name :)";
+const APP_NAME = "TS Paint";
+const CANVAS_WIDTH = 256;
+const CANVAS_HEIGHT = 256;
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const h1 = document.createElement("h1");
+h1.innerHTML = APP_NAME;
+app.append(h1);
+
+const canvas = document.createElement("canvas");
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+app.append(canvas);
+ 
