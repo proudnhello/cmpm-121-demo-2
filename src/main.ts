6d74ef7f056f13f2e15fa0a3535167555bdd096f
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-10-14 13:32:46 -0700

    added comment and edited variable names

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ const CANVAS_WIDTH = 256;
 const CANVAS_HEIGHT = 256;
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+// Creates the title of the app on the page
 document.title = APP_NAME;
-const h1 = document.createElement("h1");
-h1.innerHTML = APP_NAME;
-app.append(h1);
+const pageTitle = document.createElement("h1");
+pageTitle.innerHTML = APP_NAME;
+app.append(pageTitle);
 
+// Creates the canvas that the user will draw on
 const canvas = document.createElement("canvas");
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
